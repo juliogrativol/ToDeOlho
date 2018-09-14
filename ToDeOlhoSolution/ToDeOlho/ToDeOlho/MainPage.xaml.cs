b2a67309cdf3d1555a79dfb0386670d45011c563
jthomaz <jthomaz@brasilcap.com.br>
2018-09-14 17:20:33 -0300

    projeto inicial

---
+++ ToDeOlhoSolution/ToDeOlho/ToDeOlho/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Autenticacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToDeOlho
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void login_Clicked(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            login.email = Email_entry.Text;
+            login.senha = Senha_entry.Text;
+
+            DisplayAlert("Login", login.email, "OK");
+        }
+    }
+}
