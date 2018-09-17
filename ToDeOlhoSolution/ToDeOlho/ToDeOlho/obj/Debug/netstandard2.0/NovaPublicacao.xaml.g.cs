b6e6f1ec5e00e204ca99415830165020ecc3a227
jthomaz <jthomaz@brasilcap.com.br>
2018-09-17 18:04:07 -0300

    Salvando as publicações em memória

--- ToDeOlhoSolution/ToDeOlho/ToDeOlho/obj/Debug/netstandard2.0/NovaPublicacao.xaml.g.cs
+++ ToDeOlhoSolution/ToDeOlho/ToDeOlho/obj/Debug/netstandard2.0/NovaPublicacao.xaml.g.cs
@@ -23,9 +23,6 @@ namespace ToDeOlho {
         private global::Xamarin.Forms.Entry Titulo_entry;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
-        private global::Xamarin.Forms.Entry Foto;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private global::Xamarin.Forms.Button btn_publicar;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
@@ -33,7 +30,6 @@ namespace ToDeOlho {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(NovaPublicacao));
             BasicInfo = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "BasicInfo");
             Titulo_entry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "Titulo_entry");
-            Foto = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "Foto");
             btn_publicar = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "btn_publicar");
         }
     }
